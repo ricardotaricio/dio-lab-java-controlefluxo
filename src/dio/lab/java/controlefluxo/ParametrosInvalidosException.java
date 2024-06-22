0a71f0acd7d27025c6195e051fb34783126febe2
Ricardo Camargo Taricio <ricardotaricio@gmail.com>
2024-06-21 21:28:01 -0300

    lab java controle de fluxo

---
+++ src/dio/lab/java/controlefluxo/ParametrosInvalidosException.java
@@ -0,0 +1,13 @@
+package dio.lab.java.controlefluxo;
+
+public class ParametrosInvalidosException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -2476556846528193562L;
+	
+	public ParametrosInvalidosException(String message) {
+		super(message);
+	}
+}
